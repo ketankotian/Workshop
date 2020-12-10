4d4b4bdfaa537e3d1c7e970f4e115f47f12a3158
ketankotian <krkkotian@gmail.ocm>
2020-12-10 08:43:38 +0530

    workshop

--- Review/src/demo/Problem1.java
+++ Review/src/demo/Problem1.java
@@ -27,6 +27,8 @@ public class Problem1 {
 	}
 
 	static void evenOdd(int num) {
+//		int[] eve = new int[num];
+//		int[] odd = new int[num];
 		int evenSum = 0, oddSum = 0;
 
 		for (int i = 1; i <= num; i++) {
@@ -34,17 +36,22 @@ public class Problem1 {
 			if (i % 2 == 0) {
 				System.out.print(i + "\n");
 				evenSum = evenSum + i;
+//				eve[i] = i;
 			} else {
 				System.out.print(i + " ");
 				oddSum = oddSum + i;
+//				odd[i] = i;
 			}
 		}
 		System.out.println("\nEven Sum : " + evenSum);
+//		System.out.println("Even Array : " + eve);
 		System.out.println("Odd Sum : " + oddSum);
+//		System.out.println("Odd Array : " + odd);
 	}
 
 	static void prime(int num) {
 		int primeSum = 0, count = 0;
+//		int[] prime = new int[num];
 		for (int i = 1; i <= num; i++) {
 			for (int j = 2; j < i; j++) {
 
@@ -55,13 +62,30 @@ public class Problem1 {
 			if (count == 0) {
 				System.out.print(i + " ");
 				primeSum = primeSum + i;
+//				prime[i] = i;
 			} else {
 				count = 0;
 			}
 
 		}
 		System.out.println("\nPrime sum : " + primeSum);
+//		System.out.println("Prime Array : " + prime);
 
 	}
 
 }
+
+/*
+Enter limit : 
+7
+1 2 3 4 5 6 7 
+Sum : 28
+1 2
+3 4
+5 6
+7 
+Even Sum : 12
+Odd Sum : 16
+1 2 3 5 7 
+Prime sum : 18
+*/
